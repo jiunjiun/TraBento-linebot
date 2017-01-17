90acac4d72798e4eed7f481c5e7ee5f65684f588
jiunjiun <quietmes@gmail.com>
2017-01-17 13:28:53 +0800

    Add - Add callback routes

---
+++ app/controllers/callback_controller.rb
@@ -0,0 +1,4 @@
+class CallbackController < ApplicationController
+  def create
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
+  post :callback, to: 'callback#create', as: :callback
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
