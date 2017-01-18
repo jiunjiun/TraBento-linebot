60b5e60ac0112bd89c763bb8f994f177e5b931e5
jiunjiun <quietmes@gmail.com>
2017-01-18 11:53:44 +0800

    Add - Add Order

---
+++ app/services/line/scripts/order_script.rb
@@ -0,0 +1,166 @@
+module Line::Scripts
+  module OrderScript
+
+    module Step
+      ID_NUMBER      = 1
+      RESERVATION    = 2
+      RIBS_BOX       = 3
+      VEGETARIAN_BOX = 4
+      VAT_NUMBER     = 5
+      CAPTCHA        = 6
+      CONFIRM        = 7
+    end
+
+    def assign
+      super
+
+      return unless sourceable[:status] == Line::ScriptsBase::SourceableStatus::IDLE or sourceable[:status] == Line::ScriptsBase::SourceableStatus::ORDER
+
+      # --- parse info
+      case event
+      when Line::Bot::Event::Message
+        case event.type
+        when Line::Bot::Event::MessageType::Text
+          message = event.message['text']
+
+          if sourceable[:status] == Line::ScriptsBase::SourceableStatus::ORDER
+            case sourceable[:order][:step]
+            when Step::ID_NUMBER
+              @sourceable[:order][:params][:id] = message
+              @sourceable[:order][:step] = Line::Scripts::OrderScript::Step::RESERVATION
+              save_sourceable!
+
+              reply_messages = [
+                {
+                  type: 'text',
+                  text: "請輸入訂位預約號:"
+                },
+              ]
+            when Step::RESERVATION
+              @sourceable[:order][:params][:resNo] = message
+              @sourceable[:order][:step] = Line::Scripts::OrderScript::Step::RIBS_BOX
+              save_sourceable!
+
+              reply_messages = [
+                {
+                  type: 'text',
+                  text: "請輸入 排骨便當(80元) 數量(數量0-6之間):"
+                },
+              ]
+            when Step::RIBS_BOX
+              @sourceable[:order][:params][:ribsBox] = message.to_i
+              @sourceable[:order][:step] = Line::Scripts::OrderScript::Step::VEGETARIAN_BOX
+              save_sourceable!
+
+              reply_messages = [
+                {
+                  type: 'text',
+                  text: "請輸入 素食便當(80元) 數量(數量0-6之間):"
+                },
+              ]
+            when Step::VEGETARIAN_BOX
+              @sourceable[:order][:params][:vegetarianBox] = message.to_i
+              @sourceable[:order][:step] = Line::Scripts::OrderScript::Step::VAT_NUMBER
+              save_sourceable!
+
+              reply_messages = [
+                {
+                  type: 'text',
+                  text: "請輸入 統一編號(選填):\n若沒有請填 無"
+                },
+              ]
+            when Step::VAT_NUMBER
+              @sourceable[:order][:params][:vat] = (message == '無' ? '' : message.to_i)
+
+              @sourceable[:order][:step] = Line::Scripts::OrderScript::Step::CAPTCHA
+
+              response = TraBento.captcha
+              @sourceable[:order][:captcha_binary] = Base64.encode64(response.body)
+              @sourceable[:order][:cookies]        = response.cookies
+              save_sourceable!
+
+              reply_messages = [
+                {
+                  type: 'text',
+                  text: "請輸入驗證碼(大小寫):"
+                },
+                {
+                  type: 'image',
+                  originalContentUrl: "#{ENV['BASE_URL']}/captcha/#{@sourceable[:sourceable_id]}.jpg",
+                  previewImageUrl: "#{ENV['BASE_URL']}/captcha/#{@sourceable[:sourceable_id]}.jpg"
+                }
+              ]
+            when Step::CAPTCHA
+              @sourceable[:order][:params][:captcha] = message
+              @sourceable[:order][:step] = Line::Scripts::OrderScript::Step::CONFIRM
+              save_sourceable!
+
+              msg = ''
+              msg += "你的訂單資料為\n"
+              msg += "身分證：#{@sourceable[:order][:params][:id]}\n"
+              msg += "預約號：#{@sourceable[:order][:params][:resNo]}\n"
+              msg += "排骨便當數量：#{@sourceable[:order][:params][:ribsBox]}\n"
+              msg += "素食便當數量：#{@sourceable[:order][:params][:vegetarianBox]}\n"
+              msg += "統一編號：#{@sourceable[:order][:params][:vat]}\n"
+              msg += "\n"
+              msg += "確認無誤後，填入數字 1或2\n"
+              msg += "1: 送出訂單\n"
+              msg += "2: 取消訂單\n"
+
+              reply_messages = [
+                {
+                  type: 'text',
+                  text: msg
+                },
+              ]
+            when Step::CONFIRM
+              sourceable_idle!
+
+              if message == '1'
+                cookies = @sourceable[:order][:cookies]
+                params = @sourceable[:order][:params]
+
+                response = TraBento.order cookies, params
+                begin
+                  result = JSON.parse(response.body, symbolize_names: true)[:result]
+                rescue Exception => e
+                  result = '系統錯誤!'
+                end
+
+                reply_messages = [
+                  {
+                    type: 'text',
+                    text: result
+                  },
+                ]
+              else
+                reply_messages = [
+                  {
+                    type: 'text',
+                    text: '您已取消這次的訂單!'
+                  },
+                ]
+              end
+            end
+          elsif message =~ /^(order|Order|訂便當)/
+            sourceable_order!
+
+            reply_messages = [
+              {
+                type: 'text',
+                text: "訂便當囉!!"
+              },
+              {
+                type: 'text',
+                text: "請輸入身份證:"
+              }
+            ]
+          end
+
+          response = client.reply_message(event['replyToken'], reply_messages)
+        end
+      end
+    end
+
+  end
+end
--- app/services/line/scripts_base.rb
+++ app/services/line/scripts_base.rb
@@ -2,6 +2,7 @@ module Line
   class ScriptsBase < Base
     prepend Line::Scripts::HelpScript
     prepend Line::Scripts::SearchScript
+    prepend Line::Scripts::OrderScript
 
     attr_reader :event
     attr_reader :sourceable
@@ -9,6 +10,7 @@ module Line
     module SourceableStatus
       IDLE   = 0
       SEARCH = 1
+      ORDER  = 2
     end
 
     def self.assign event
@@ -28,23 +30,24 @@ module Line
       case source['type']
       when 'user'
         user_id = source['userId']
-        result = $redis.get "u#{user_id}"
+        sourceable_id = "u#{user_id}"
       when 'group'
         group_id = source['groupId']
-        result = $redis.get "g#{group_id}"
+        sourceable_id = "g#{user_id}"
       when 'room'
         room_id = source['roomId']
-        result = $redis.get "r#{room_id}"
+        sourceable_id = "r#{user_id}"
       end
+      result = $redis.get sourceable_id
 
       if result.present?
         @sourceable = JSON.parse(result, symbolize_names: true)
       else
-        @sourceable = { status: Line::ScriptsBase::SourceableStatus::IDLE }
+        @sourceable = { status: Line::ScriptsBase::SourceableStatus::IDLE, sourceable_id: sourceable_id}
         save_sourceable!
       end
     rescue
-      @sourceable = { status: Line::ScriptsBase::SourceableStatus::IDLE }
+      @sourceable = { status: Line::ScriptsBase::SourceableStatus::IDLE, sourceable_id: sourceable_id}
       save_sourceable!
     end
 
@@ -71,6 +74,24 @@ module Line
 
     def sourceable_search!
       @sourceable[:status] = Line::ScriptsBase::SourceableStatus::SEARCH
+      save_sourceable!
+    end
+
+    def sourceable_order!
+      @sourceable[:status] = Line::ScriptsBase::SourceableStatus::ORDER
+      @sourceable[:order] = {
+        step: Line::Scripts::OrderScript::Step::ID_NUMBER,
+        cookies: nil,
+        captcha_binary: nil,
+        params: {
+          captcha: '',
+          id: '',
+          resNo: '',
+          ribsBox: '',
+          vegetarianBox: '',
+          vat: '',
+        }
+      }
       save_sourceable!
     end
   end
